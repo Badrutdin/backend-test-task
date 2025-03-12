2a0edae3dc5f03575d9d89b791f3ec145110ea05
Badrutdin <86923895+Badrutdin@users.noreply.github.com>
2025-03-12 15:54:58 +0300

    Update CalculateController.php

--- src/Controller/CalculateController.php
+++ src/Controller/CalculateController.php
@@ -5,20 +5,21 @@ namespace App\Controller;
 use App\DTO\CalculatePriceRequest;
 use App\Service\PriceCalculator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 class CalculateController extends AbstractController
 {
     public function __construct(
-        private PriceCalculator $priceCalculator,
+        private PriceCalculator     $priceCalculator,
         private SerializerInterface $serializer,
-        private ValidatorInterface $validator
-    ) {}
+        private ValidatorInterface  $validator
+    )
+    {
+    }
 
     #[Route('/calculate-price', name: 'app_calculate_price', methods: ['POST'])]
     public function calculatePrice(Request $request): Response
@@ -37,7 +38,7 @@ class CalculateController extends AbstractController
                 foreach ($violations as $violation) {
                     $errorMessages[$violation->getPropertyPath()] = $violation->getMessage() . ' Received: ' . $violation->getInvalidValue();
                 }
-                return new JsonResponse(['errors' => $errorMessages], Response::HTTP_BAD_REQUEST);
+                return $this->json(['errors' => $errorMessages], Response::HTTP_BAD_REQUEST);
             }
 
             $price = $this->priceCalculator->calculate(
@@ -46,10 +47,10 @@ class CalculateController extends AbstractController
                 $calculateRequest->getCouponCode()
             );
 
-            return new JsonResponse(['price' => $price]);
+            return $this->json(['price' => $price]);
 
         } catch (\Exception $e) {
-            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         }
     }
 }
